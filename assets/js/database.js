2e1d9c12cf60dce11439cbdfaea562807911185c
Robson Hansen <robsonrudson65@gmail.com>
2022-07-28 00:01:48 -0300

    commit1

---
+++ assets/js/database.js
@@ -0,0 +1,367 @@
+// Banco de dados dos produtos
+
+const data = [
+  {
+    id: 1,
+    img: "/assets/img/pokemon/ultrararas/1-CHARIZARD.png",
+    nameCard: "Charizard",
+    description:
+      "Charizard (#136-135)",
+    value: 1599.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 2,
+    img: "/assets/img/pokemon/ultrararas/2-Groudon-EX.png",
+    nameCard: "Groundon EX",
+    description:
+      "Groundon-EX (#106-108)",
+    value: 303.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 3,
+    img: "/assets/img/pokemon/ultrararas/3-Lugia-EX.png",
+    nameCard: "Lugya EX",
+    description:
+      "Lugya-EX (#94-98)",
+    value: 239.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 4,
+    img: "/assets/img/pokemon/ultrararas/4-Charizard-VMAX.jpg",
+    nameCard: "Charizard VMAX",
+    description:
+      "Charizard-VMAX (#20-189)",
+    value: 223.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 5,
+    img: "/assets/img/pokemon/ultrararas/5-Greninja-GX.jpg",
+    nameCard: "Grenija GX",
+    description:
+      "Grenija-GX (#133-131)",
+    value: 223.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 6,
+    img: "/assets/img/pokemon/ultrararas/6-Rayquaza-EX.png",
+    nameCard: "Rayquaza EX",
+    description:
+      "Rayquaza-EX (#123-124)",
+    value: 199.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 7,
+    img: "/assets/img/pokemon/ultrararas/7-CALYREX_CAVALEIRO_ESPECTRAL.jpg",
+    nameCard: "Calyrrex Cavaleiro Espectral",
+    description:
+      "Calyrrex-Cavaleiro Espectral (#172-198)",
+    value: 159.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 8,
+    img: "/assets/img/pokemon/ultrararas/8-Raikou-EX.png",
+    nameCard: "Raikou EX",
+    description:
+      "Raikou-EX (#105-108)",
+    value: 159.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 9,
+    img: "/assets/img/pokemon/ultrararas/9-Noivern-V.jpg",
+    nameCard: "Noivern V",
+    description:
+      "Noivern-V (#196-203)",
+    value: 159.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 10,
+    img: "/assets/img/pokemon/ultrararas/10-Mewtwo-EX.png",
+    nameCard: "Mewtwo EX",
+    description:
+      "Mewtwo-EX (#158-162)",
+    value: 79.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 11,
+    img: "/assets/img/yugioh/ultrarare/1-PEQUENAMAGANEGRA.jpg",
+    nameCard: "Pequena Maga Negra",
+    description:
+      "Dark Magician Girl(#MFC-000)",
+    value: 2099.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 12,
+    img: "/assets/img/yugioh/ultrarare/2-MAGONEGRO.jpg",
+    nameCard: "Mago Negro",
+    description:
+      "Dark Magician (#25TH-EN001)",
+    value: 1099.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 13,
+    img: "/assets/img/yugioh/ultrarare/3-RAVIEL.jpg",
+    nameCard: "Raviel",
+    description:
+      "Raviel, Lord of Phantasms (#SOI-EN003)",
+    value: 849.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 14,
+    img: "/assets/img/yugioh/ultrarare/4-GUERREIRO-SHIBA-TARO.jpg",
+    nameCard: "Guerreiro Shiba Taro",
+    description:
+      "Shiba-Warrior Taro (#YAP1-EN008)",
+    value: 699.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 15,
+    img: "/assets/img/yugioh/ultrarare/5-ELEMENTAL-HERO-AIR-NEOS.png",
+    nameCard: "Elemental Hero Air Neos",
+    description:
+      "Elemental Hero Air Neos (#STON-EN034)",
+    value: 79.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 16,
+    img: "/assets/img/yugioh/ultrarare/6-DRAGÃO-CAVEIRA-NEGRO.png",
+    nameCard: "Dragão Cavaleiro Negro",
+    description:
+      "B.Skull Dragon(#MRD-018)",
+    value: 499.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 17,
+    img: "/assets/img/yugioh/ultrarare/7-DRAGAO-NEGRO-DE-OLHOS-VERMELHOS.jpg",
+    nameCard: "Dragão Negro de Olhos Vermelhos",
+    description:
+      "Red-Eyes B.Dragon(#LOB-070)",
+    value: 449.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 18,
+    img: "/assets/img/yugioh/ultrarare/8-POTE-DA-EXTRAVAGANCIA.jpg",
+    nameCard: "Pote da Extravagância",
+    description:
+      "Pot of Extravagance(#TOCH-EN059)",
+    value: 449.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 19,
+    img: "/assets/img/yugioh/ultrarare/9-EXODIA.jpg",
+    nameCard: "Exodia",
+    description:
+      "Exodia The Forbidden One(#LOB-124)",
+    value: 299.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 20,
+    img: "/assets/img/yugioh/ultrarare/10-DRAGAO-TOON-DE-OLHOS-AZUIS.png",
+    nameCard: "Dragão Toon de Olhos Azuis",
+    description:
+      "Blue-Eyes Toon Dragon(#SRL-000)",
+    value: 249.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 21,
+    img: "/assets/img/magic/mitic/1-oDeusEscaravelhoTheScarabGod.jpg",
+    nameCard: "O Deus Escaravelho",
+    description:
+      "The Scarab God",
+    value: 1350.00,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 22,
+    img: "/assets/img/magic/mitic/2-aImperatrizErrante.jpg",
+    nameCard: "A Imperatriz Errante",
+    description:
+      "The Wandering Emperor(#303)",
+    value: 358.00,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 23,
+    img: "/assets/img/magic/mitic/3-dragãodeCobreAnciao.jpg",
+    nameCard: "Dragão de Cobre Ancião",
+    description:
+      "Ancient Copper Dragon",
+    value: 292.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 24,
+    img: "/assets/img/magic/mitic/4-bosqueAlagado.jpg",
+    nameCard: "Bosque Alagado",
+    description:
+      "Flooded Grove",
+    value: 269.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 25,
+    img: "/assets/img/magic/mitic/5-karnLiberto.jpg",
+    nameCard: "Karn Liberto",
+    description:
+      "Karn Liberated",
+    value: 245.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 26,
+    img: "/assets/img/magic/mitic/6-regenteMarturvo.jpg",
+    nameCard: "Regente Martuvo",
+    description:
+      "Murktide Regent",
+    value: 229.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 27,
+    img: "/assets/img/magic/mitic/7-vorinclexMonstrousRaider.jpg",
+    nameCard: "Vorinclex, Salteador Monstruoso",
+    description:
+      "Vorinclex, Monstrous Raider",
+    value: 199.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 28,
+    img: "/assets/img/magic/mitic/8-colossoCultivador.jpg",
+    nameCard: "Colosso Cultivador",
+    description:
+      "Cultivador colossus",
+    value: 122.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 29,
+    img: "/assets/img/magic/mitic/9-dackFayden.jpg",
+    nameCard: "Dack Fayden",
+    description:
+      "Dack Fayden",
+    value: 109.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 30,
+    img: "/assets/img/magic/mitic/10-kairi.jpg",
+    nameCard: "Kairi, o Céu Turbilhonante (#408)",
+    description:
+      "Kairi, the Swirling Sky (#408)",
+    value: 99.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  //novos Cards
+  {
+    id: 31,
+    img: "/assets/img/pokemon/ultrararas/11-PIKACHU.jpg",
+    nameCard: "Pikachu",
+    description:
+      "Pikachu (#RC29/83)",
+    value: 95.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 32,
+    img: "/assets/img/pokemon/ultrararas/12-ZOROARK-GX.jpg",
+    nameCard: "Zoroark-GX",
+    description:
+      "Zoroark-GX (#77/73)",
+    value: 79.80,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Pokémon"],
+  },
+  {
+    id: 33,
+    img: "/assets/img/yugioh/ultrarare/11-VIRA-PÁGINAS-TOON.jpg",
+    nameCard: "Vira-Páginas Toon",
+    description:
+      "Toon Page-Flip(#TOCH-EN004)",
+    value: 220.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 34,
+    img: "/assets/img/yugioh/ultrarare/12-DRAGÃO-Azul.png",
+    nameCard: "Dragão Branco de Olhos Azuis",
+    description:
+      "Blue-Eyes White Dragon(#JMP-001)",
+    value: 219.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Yu-Gi-Oh"],
+  },
+  {
+    id: 35,
+    img: "/assets/img/magic/mitic/11-kaito.jpg",
+    nameCard: "Keito Shizuki",
+    description:
+      "Kaito Shizuki",
+    value: 89.95,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+  {
+    id: 36,
+    img: "/assets/img/magic/mitic/12-tarmogoyf.jpg",
+    nameCard: "Tarmogoyf",
+    description:
+      "Tarmogoyf",
+    value: 89.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Magic"],
+  },
+
+
+];
---
+++ assets/js/magic.js
@@ -0,0 +1,222 @@
+const listMagic = document.querySelector('#lista-magic');
+
+function listarCards(listdatabase) {
+    for (let i = 0; i < listdatabase.length; i++) {
+        let card = listdatabase[i];
+
+        if (card.tag[0] === "Magic") {
+            criarCardMagic(card);
+
+        }
+
+    }
+}
+
+function criarCardMagic(database) {
+    let idTag = database.id;
+    let categoria = database.tag;
+    let cardNome = database.nameCard;
+    let cardImg = database.img;
+    let cardDescricao = database.description;
+    let cardPrice = database.value;
+    let cardAddCart = database.addCart;
+
+    let tagLi = document.createElement('li');
+    tagLi.setAttribute('id', idTag);
+    let tagDiv1 = document.createElement('div');
+    tagDiv1.setAttribute('class', 'imagem-card');
+    let tagImg = document.createElement('img');
+    tagImg.setAttribute('src', cardImg);
+    tagImg.setAttribute('alt', cardNome);
+    tagDiv1.appendChild(tagImg);
+    let tagDiv2 = document.createElement('div');
+    tagDiv2.setAttribute('class', 'conteudo');
+    let tagSpan = document.createElement('span');
+    tagSpan.setAttribute('class', 'conteudo-categiria');
+    tagSpan.innerHTML = categoria;
+    let tagH2 = document.createElement('h2');
+    tagH2.setAttribute('class', 'conteudo-card-nome');
+    tagH2.innerHTML = cardNome;
+    let tagP1 = document.createElement('p');
+    tagP1.setAttribute('class', 'conteudo-descricao');
+    tagP1.innerHTML = `<em>${cardDescricao}</em>`;
+    let tagP2 = document.createElement('p');
+    tagP2.setAttribute('class', 'conteudo-price');
+    tagP2.innerHTML = `R$ ${cardPrice.toFixed(2).replace('.', ',')}`;
+    let tagButton = document.createElement('button');
+    tagButton.setAttribute('class', 'conteudo__card-button');
+    tagButton.innerHTML = cardAddCart;
+
+
+    tagDiv2.appendChild(tagSpan);
+    tagDiv2.appendChild(tagH2);
+    tagDiv2.appendChild(tagP1);
+    tagDiv2.appendChild(tagP2);
+    tagDiv2.appendChild(tagButton);
+    tagLi.appendChild(tagDiv1);
+    tagLi.appendChild(tagDiv2);
+    listMagic.appendChild(tagLi);
+}
+
+listarCards(data);
+
+//add ao carrinho
+
+const listaCarrinho = document.querySelector('.lista-carrinho');
+const addCarrinhoButton = document.querySelectorAll('.conteudo__card-button');
+const carrinhoVazio = document.querySelector('#carrinho-vazio');
+
+
+function addCarrinho(database) {
+    let idTag = database.id;
+    let cardNome = database.nameCard;
+    let cardImg = database.img;
+    let cardPrice = database.value;
+
+    let tagLi = document.createElement('li');
+    tagLi.setAttribute('class', 'card-carrinho');
+    tagLi.setAttribute('id', idTag);
+    let tagDiv1 = document.createElement('div');
+    tagDiv1.setAttribute('class', 'img__card-conteudo');
+    let tagImg = document.createElement('img');
+    tagImg.setAttribute('class', 'imagem-card-conteudo-img ');
+    tagImg.setAttribute('src', cardImg);
+    tagImg.setAttribute('alt', cardNome);
+    tagDiv1.appendChild(tagImg);
+    let tagDiv2 = document.createElement('div');
+    tagDiv2.setAttribute('class', 'conteiner__conteudo-carrinho');
+    let tagH4 = document.createElement('h4');
+    tagH4.setAttribute('class', 'conteudo-card-nome-carrinho');
+    tagH4.innerHTML = cardNome;
+    let tagP = document.createElement('p');
+    tagP.setAttribute('class', 'conteudo-price-carrinho');
+    tagP.innerHTML = `R$ ${cardPrice.toFixed(2).replace('.', ',')}`;
+    let tagButton = document.createElement('button');
+    tagButton.setAttribute('class', 'remover-card-button');
+    tagButton.innerHTML = 'Remover Carta';
+
+
+    tagDiv2.appendChild(tagH4);
+    tagDiv2.appendChild(tagP);
+    tagDiv2.appendChild(tagButton);
+    tagLi.appendChild(tagDiv1);
+    tagLi.appendChild(tagDiv2);
+    listaCarrinho.appendChild(tagLi);
+
+}
+
+let listaTotal = [];
+
+
+
+function removerCarrinhoVazio() {
+
+    for (let i = 0; i < addCarrinhoButton.length; i++) {
+
+        if (listaCarrinho.classList.toggle('carrinho-vazio') === true) {
+
+            carrinhoVazio.remove()
+            document.querySelector('.carrinho').insertAdjacentHTML('beforeend',
+                `<div class="qtd-total">
+                        <div id="qtd-estoque">
+                            <h4>Quantidade:</h4>
+                            <p id="qtd-carrinho"></p>
+                        </div>
+                        <div id="total-compra">
+                            <h4>Total:</h4>
+                            <p id="total-valor"></p>
+                        </div>
+                    </div>
+                    <button id="finalizar-compra">Finalizar Compra</button>`
+            );
+        } else if (listaCarrinho.classList.toggle('carrinho-vazio') === false) {
+            //não faz nada
+        }
+        totalCompra(listaTotal);
+
+
+    }
+
+}
+
+function totalCompra(listaTotal) {
+    let total = 0;
+    for (let i = 0; i < listaTotal.length; i++) {
+        total += parseFloat(listaTotal[i]);
+    }
+}
+
+function contQtdTotal() {
+    let qtdCard = document.querySelector('#qtd-carrinho');
+    let qtdCarrinho = document.querySelectorAll('.card-carrinho');
+
+    qtdCard.innerHTML = qtdCarrinho.length + 1;
+
+    if (qtdCarrinho.length === 0) {
+        qtdCard.innerHTML = qtdCard.outerHTML;
+    } else {
+        qtdCard.innerHTML = qtdCarrinho.length + 1;
+    }
+
+}
+
+function calcValorTotal() {
+    // const valorCard = document.querySelectorAll('.conteudo-price-carrinho');
+
+    // for (let i = 0; i < valorCard.length; i++) {
+    //     listaTotal.push(valorCard[i].innerHTML.substring(3, valorCard[i].innerHTML.length - 1).replace(',', '.'));
+    // }
+    let totalCard = document.querySelector('#total-valor');
+    let valorTotal = 0;
+    let valorCard = document.querySelectorAll('.conteiner__conteudo-carrinho p');
+
+    if (valorTotal === 0) {
+
+        for (let i = 0; i < valorCard.length; i++) {
+            valorTotal += parseFloat(valorCard[i].innerHTML.substring(3, valorCard[i].innerHTML.length - 1).replace(',', '.'));
+        }
+        for (let i = 0; i < valorCard.length; i++) {
+            let valorDoCard = valorCard[i].innerHTML.substring(3, valorCard[i].innerHTML.length - 1).replace(',', '.');
+            valorTotal += parseFloat(valorDoCard);
+        }
+    }
+
+
+    totalCard.innerHTML = `R$ ` + valorTotal.toFixed(2).replace('.', ',');
+
+}
+
+
+
+for (let i = 0; i < addCarrinhoButton.length; i++) {
+    addCarrinhoButton[i].addEventListener('click', function () {
+        removerCarrinhoVazio();
+        contQtdTotal();
+        calcValorTotal();
+        addCarrinho(data[i]);
+    }
+    );
+
+}
+
+//esvaziar o carrinho 
+
+const botaoRemoverCard = document.querySelectorAll('.lista-carrinho');
+const qtdTotal = document.querySelector('.qtd-total');
+const qtdCarrinho = document.querySelector('#qtd-carrinho');
+const buttonFinalizarCompra = document.querySelector('#finalizar-compra');
+
+for (let i = 0; i < botaoRemoverCard.length; i++) {
+    botaoRemoverCard[i].addEventListener('click', function (event) {
+        botaoRemover = event.target;
+
+        if (botaoRemover.classList.contains('remover-card-button') === true) {
+            botaoRemover.parentElement.parentElement.remove();
+            contQtdTotal();
+            calcValorTotal();
+            removerCarrinhoVazio();
+        } else {
+            location.reload();
+        }
+    });
+}
